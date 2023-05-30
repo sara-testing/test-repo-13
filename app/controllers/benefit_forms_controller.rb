a7f431d94fda7e8bf7e7e011dce1ec30cffcf896
Sara Lavanchy <saralavanchy94@gmail.com>
2023-05-30 13:38:15 -0400

    innitial commit for pii

---
+++ app/controllers/benefit_forms_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+class BenefitFormsController < ApplicationController
+
+  def index
+    @benefits = Benefits.new
+  end
+
+  def download
+   begin
+     path = params[:name]
+     file = params[:type].constantize.new(path)
+     send_file file, disposition: "attachment"
+   rescue
+     redirect_to user_benefit_forms_path(user_id: current_user.id)
+   end
+  end
+
+  def upload
+    file = params[:benefits][:upload]
+    if file
+      flash[:success] = "File Successfully Uploaded!"
+      Benefits.save(file, params[:benefits][:backup])
+    else
+      flash[:error] = "Something went wrong"
+    end
+    redirect_to user_benefit_forms_path(user_id: current_user.id)
+  end
+
+  def delete
+    file = params[:benefits][:delete]
+    if file
+      flash[:success] = "File Successfully Deleted!"
+      Benefits.delete(file, params[:benefits][:backup])
+    else
+      flash[:error] = "Something went wrong"
+    end
+    redirect_to user_benefit_forms_path(user_id: current_user.id)
+  end
+
+end
